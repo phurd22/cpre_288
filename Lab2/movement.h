b9a6e9780318957dae1f072534f3783e1b241d55
phurd22 <phurd22@iastate.edu>
2024-01-29 22:56:35 -0600

    Update movement.h

--- Lab2/movement.h
+++ Lab2/movement.h
@@ -3,5 +3,10 @@
 #define HEADER_FILE
 
 //Function headers and macro definitions
+double move_forward(oi_t*sensor_data, double distance_mm);
+
+double turn_right(oi_t*sensor, double degrees);
+
+double turn_left(oi_t*sensor, double degrees)
 
 #endif
