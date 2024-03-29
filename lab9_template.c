676ebdba944d727962ad2809f388a5dd0d89d186
dmaheeka <123406094+dmaheeka@users.noreply.github.com>
2024-03-29 14:25:20 -0500

    Add files via upload

---
+++ lab9_template.c
@@ -0,0 +1,35 @@
+/**
+ * @file lab9_template.c
+ * @author
+ * Template file for CprE 288 Lab 9
+ */
+
+#include "Timer.h"
+#include "lcd.h"
+#include "ping.h"
+
+// Uncomment or add any include directives that are needed
+
+#warning "Possible unimplemented functions"
+#define REPLACEME 0
+
+int main(void) {
+	timer_init(); // Must be called before lcd_init(), which uses timer functions
+	lcd_init();
+ 	ping_init();
+
+	// YOUR CODE HERE
+	//ping_trigger();
+
+	while(1)
+	{
+
+      // YOUR CODE HERE
+
+	    float dist = 0;
+	    dist = ping_getDistance();
+	    lcd_printf("%.2f, OF: %d", dist, overflow);
+	    timer_waitMicros(500000);
+	}
+
+}
